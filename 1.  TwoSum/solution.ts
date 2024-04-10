f684ef6772aafb94972a8c6c653d67bd0d25162d
llindemann <leonie.lindemann.ll@gmail.com>
2024-04-10 21:05:59 +0200

    doc: explaination of TwoSum solution

--- 1.  TwoSum/solution.ts
+++ 1.  TwoSum/solution.ts
@@ -2,12 +2,18 @@ function twoSum(nums: number[], target: number): number[] | undefined {
     const indices = new Map();
 
     for (let index = 0; index < nums.length; index++) {
+
+        //get the complement value of current num
         const complement = target - nums[index];
 
+        //check if the hash already stores the complement
         if (indices.has(complement)) {
+
+            // return num & complement 
             return [indices.get(complement), index]
         }
 
+        // add num in hash map, when complement not yet found
         indices.set(nums[index], index)
     }
 
