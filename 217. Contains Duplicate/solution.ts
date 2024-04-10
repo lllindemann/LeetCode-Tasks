4ccfecf5c25fa6501ce66c4d439192f584143217
llindemann <leonie.lindemann.ll@gmail.com>
2024-04-10 21:20:26 +0200

    feat: Contains Duplicate Problem

---
+++ 217. Contains Duplicate/solution.ts
@@ -0,0 +1,21 @@
+function containsDuplicate(nums: number[]): boolean {
+
+    const indices = new Set();
+
+    for (let index = 0; index < nums.length; index++) {
+
+        //check if the Hash Set already stores the num
+        if (indices.has(nums[index])) {
+
+            // return true if there is a Duplicate
+            return true;
+        }
+
+        // add num to Hash Set map, when a duplicate not yet found
+        indices.add(nums[index])
+    }
+
+    // return false if there is no a Duplicate
+    return false;
+
+};
